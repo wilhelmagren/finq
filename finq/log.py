796a185206a36da873d98f34994f1895a3d48131
willeagren <wilhelmagren98@gmail.com>
2023-10-18 23:21:14 +0200

    feat(log): implement colored log formatter

--- finq/log.py
+++ finq/log.py
@@ -22,23 +22,54 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 File created: 2023-10-15
-Last updated: 2023-10-15
+Last updated: 2023-10-18
 """
 
 import logging
+from enum import Enum
 
 
-def get_module_log(
-    name: str,
-    extra_name: str = "module_name",
-) -> logging.Logger:
+class Color(Enum):
+    BLACK = "\x1b[0;30m"
+    RED = "\x1b[0;31m"
+    GREEN = "\x1b[0;32m"
+    YELLOW = "\x1b[0;33m"
+    BLUE = "\x1b[0;34m"
+    PURPLE = "\x1b[0;35m"
+    CYAN = "\x1b[0;36m"
+    WHITE = "\x1b[0;37m"
+
+    BOLD_RED = "\x1b[1;31m"
+    UNDERLINE_RED = "\x1b[4;31m"
+    INTENSE_RED = "\x1b[0;91m"
+    INTENSE_BOLD_RED = "\x1b[1;91m"
+
+    RESET = "\x1b[0m"
+
+
+class ColoredFormatter(logging.Formatter):
     """ """
-    parts = name.split(".")
-    module_name = parts[0]
-    if len(parts) > 1:
-        module_name += f"..{parts[-1]}" if len(parts) > 2 else f".{parts[1]}"
 
-    return logging.LoggerAdapter(
-        logging.getLogger(name),
-        {extra_name: module_name},
-    )
+    def __init__(self, format: str):
+        """ """
+        super(ColoredFormatter, self).__init__()
+
+        self._formats = {
+            logging.DEBUG: Color.GREEN.value + format + Color.RESET.value,
+            logging.INFO: Color.WHITE.value + format + Color.WHITE.value,
+            logging.WARNING: Color.YELLOW.value + format + Color.RESET.value,
+            logging.ERROR: Color.RED.value + format + Color.RESET.value,
+            logging.CRITICAL: Color.BOLD_RED.value + format + Color.RESET.value,
+        }
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ """
+
+        log_format = self._formats.get(record.levelno, None)
+        if log_format is None:
+            raise ValueError(
+                f"Unexpected log level found in provided logRecord, `{record.levelno}`.",
+            )
+
+        formatter = logging.Formatter(log_format)
+        return formatter.format(record)
