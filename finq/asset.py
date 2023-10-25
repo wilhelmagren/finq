1ed90c4b1ab9c8e34c33c45e690a769600c55753
wilhelmagren <wilhelmagren98@gmail.com>
2023-10-25 17:19:42 +0200

    feat(asset): implement quantities

--- finq/asset.py
+++ finq/asset.py
@@ -22,10 +22,11 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 File created: 2023-10-18
-Last updated: 2023-10-21
+Last updated: 2023-10-25
 """
 
 import logging
+import numpy as np
 import pandas as pd
 from typing import (
     Optional,
@@ -53,3 +54,19 @@ class Asset(object):
         self._market = market
         self._index_name = index_name
         self._price_type = price_type
+
+    def period_returns(self, period: int = 1) -> pd.Series:
+        """ """
+        return self._data.pct_change(periods=period)
+
+    def period_returns_mean(self, period: int = 1) -> np.float32:
+        """ """
+        return self.period_returns().mean(axis=0).as_numpy().astype(np.float32)
+
+    def volatility(self, period: int = 1) -> np.float32:
+        """ """
+        return self.period_returns().std() * np.sqrt(period)
+
+    def skewness(self) -> np.float32:
+        """ """
+        return self._data.skew()
